58b18f1b9c6ba7a5add6dfb36eb60cd9afa99020
andrewsmith289 <andrew.c.smith289@gmail.com>
2019-07-30 21:39:28 -0400

    Added htaccess for rewrites and added encryption settings file.

---
+++ crypt.php
@@ -0,0 +1,9 @@
+<?php
+if ( !defined( 'STRAIN_MANAGER_PATH' ) ) {
+    define( 'STRAIN_MANAGER_PATH', '/var/www/html/wp-content/plugins/strain-manager/' );
+}
+
+define( 'BCBS_PUB_KEY_NAME', 'bcbs-dev.pub' );
+define( 'BCBS_PUB_KEY_PATH', STRAIN_MANAGER_PATH . BCBS_PUB_KEY_NAME );
+define( 'IV_LENGTH', 16 );
+define( 'UM_BASE_DIR', '/var/www/html/wp-content/uploads/ultimatemember/' );
